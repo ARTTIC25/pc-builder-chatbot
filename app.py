7a2077a3d3f200bdb8f7b32a9d9651fe333b3f00
ARTTIC25 <amithpanil252006@gmail.com>
2026-06-25 16:01:56 +0530

    flask basics

--- app.py
+++ app.py
@@ -6,31 +6,11 @@ from flask import Flask,render_template
 import json
 import re
 
-message=input("You:")
-numbers=re.findall(r"\d+",message)
+app= Flask(__name__)
 
-if not numbers:
-    print("bot: please provide a budget")
-pp=purpose(message)
-comp=detecter(message)
-print("Detected purpose:",pp)
-print("Detected gpu by user:",comp)
+@app.route("/")
+def home():
+    return render_template("index.html")
+if __name__=="__main__":
+    app.run(debug=True)
 
-budget=int(numbers[0])
-build= recommend(budget,pp,comp)
-comp=compact(build["cpu"],build["motherboard"],build["ram"])
-total_price=build["cpu"]["price"]+build["gpus"]["price"]+build["ram"]["price"]+build["motherboard"]["price"]
-
-print("\nbot:")
-print("Loading......\n")
-print("======Recommended System========")
-print(f"Cpu:{build["cpu"]["name"]}:-{build["cpu"]["price"]}")
-print(f"Gpus:{build["gpus"]["name"]}:-{build["gpus"]["price"]}")
-print(f"Ram:{build["ram"]["name"]}:-{build["ram"]["price"]}")
-print(f"Motherboard:{build["motherboard"]["name"]}:-{build["motherboard"]["price"]}\n")
-
-print("Total price:-",total_price)
-if comp:
-    print("Compatible Build")
-else:
-    print("Incompatible Build")
