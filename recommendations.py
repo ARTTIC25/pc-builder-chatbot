fd7a0988b519af3edd4781698ac48041054e17b8
ARTTIC25 <amithpanil252006@gmail.com>
2026-06-28 20:44:09 +0530

    fixed partial recommendation

--- recommendations.py
+++ recommendations.py
@@ -7,39 +7,56 @@ cpus = load_json("data/cpus.json")
 gpus = load_json("data/gpus.json")
 motherboards = load_json("data/motherboards.json")
 rams = load_json("data/ram.json")
+ssds=load_json("data/psu.json")
+psus=load_json("data/psu.json")
+cabinets=load_json("data/cabinets.json")
+coolers=load_json("data/coolers.json")
+
+
 def recommend(budget, purpose,pref_gpu):
-    ram =components["ram"][0]
+    selected_ssd = ssds[0]
 
-    if purpose == "gaming":
+    candidate_cpus=[]
+    for cpu in cpus:
+        if purpose in cpu["purpose"]:
+            if cpu["price"]<=budget *0.25:
+                candidate_cpus.append(cpu)
+    cpu=max(candidate_cpus,key= lambda x :x["cores"])
 
-        if budget <= 50000:
-            cpu = components["cpus"][0]
-            gpus = components["gpus"][0]
-
-        else:
-            cpu = components["cpus"][2]
-            gpus = components["gpus"][2]
-
-    elif purpose == "editing":
-
-        cpu = components["cpus"][1]
-        gpus = components["gpus"][0]
-
-    else:
-
-        cpu = components["cpus"][0]
-        gpus = components["gpus"][0]
+    
+    candidate_gpus = []
+    for gpu in gpus:
+        if purpose in gpu["purpose"]:
+            if gpu["price"] <= budget * 0.40:
+                candidate_gpus.append(gpu)
 
     if pref_gpu:
-        for g in components["gpus"]:
-            if g["name"]==pref_gpu:
-                gpus=g
+        for gpu in gpus:
+            if gpu["name"] == pref_gpu:
+                selected_gpu = gpu
+                break
+    selected_gpu = max(candidate_gpus,key=lambda x: x["vram"])
     
+    for board in motherboards:
+        if board["socket"] == cpu["socket"]:
+            motherboard = board
+            break
 
-    if cpu["socket"]=="AM4":
-        motherboard=components["motherboards"][0]
-    else:
-        motherboard=components["motherboards"][1]
+    for memory in rams:
+        if memory["ram_type"] == motherboard["ram_type"]:
+            ram = memory
+            break
+    for ssd in ssds:
+        if ssd["capacity"] == user_storage:
+            selected_ssd = ssd
+            break
+
+    required = gpu["recommended_psu"]
+    for psu in psus:
+        if psu["wattage"] >= required:
+            selected_psu = psu
+            break
+    
 
     return {
         "cpu": cpu,
